e1320fac5b879c0de7463739c8810b20c7f0825e
duydpm225407-art <duy_dpm225407@student.agu.edu.vn>
2025-10-06 09:12:54 +0700

    Add files via upload

---
+++ DPM225407_NguyenHuuDuy_Patterns_04_Prototype/ConcretePrototype1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225407_NguyenHuuDuy_Patterns_04_Prototype
+{
+    class ConcretePrototype1 : Prototype
+    {
+        public ConcretePrototype1(string id)
+            : base(id)
+        {
+        }
+        // Returns a shallow copy
+        public override Prototype Clone()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+    }
+}
