67968576aaebe4673290c618836794598c4d192a
phuongdpm225459-ux <phuong_dpm225459@student.agu.edu.vn>
2025-10-06 10:25:00 +0700

    Add files via upload

---
+++ DPM225459_ToThiMyPhuong_MyWorld_Singleton/DPM225459_ToThiMyPhuong_MyWorld_Singleton/Room.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225459_ToThiMyPhuong_MyWorld_Singleton
+{
+    public class Room
+    {
+        public int RoomNumber { get; set; }
+        public string RoomType { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public Room(int number, string type, bool available)
+        {
+            RoomNumber = number;
+            RoomType = type;
+            IsAvailable = available;
+        }
+
+        public interface IRoomCollection
+        {
+            RoomIterator CreateIterator();
+        }
+    }
+}
