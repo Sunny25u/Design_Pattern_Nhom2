5f5a28af32ae0987cc09ed18d75945669bccd069
Quốc Thanh <quocthanh2614@gmail.com>
2025-10-06 09:26:35 +0700

    Add files via upload

---
+++ DPM225480_DinhQuocThanh_Pattern13_Chain of Responsibility/DPM225480_DinhQuocThanh_Pattern13_Chain of Responsibility/ConcreteHandler1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225480_DinhQuocThanh_Pattern13_Chain_of_Responsibility
+{
+    public class ConcreteHandler1 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 0 && request < 10)
+            {
+                Console.WriteLine("{0} handled request {1}",
+                    this.GetType().Name, request);
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+
+
+}
