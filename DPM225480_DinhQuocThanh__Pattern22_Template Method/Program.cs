5496e9a8af0e38774ba9cf693bbca03a2b942d6f
Quốc Thanh <quocthanh2614@gmail.com>
2025-10-06 09:02:27 +0700

    Add files via upload

---
+++ DPM225480_DinhQuocThanh__Pattern22_Template Method/Program.cs
@@ -0,0 +1,14 @@
+﻿using DPM225480_DinhQuocThanh__Pattern22_Template_Method;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        AbstractClass aA = new ConcreteClassA();
+        aA.TemplateMethod();
+        AbstractClass aB = new ConcreteClassB();
+        aB.TemplateMethod();
+        // Wait for user
+        Console.ReadKey();
+    }
+}
