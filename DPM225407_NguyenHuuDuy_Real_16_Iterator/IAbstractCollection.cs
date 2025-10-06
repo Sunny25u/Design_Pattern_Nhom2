79596d0fa9e7a1b4dbe8037b2d8bd7355d203d85
duydpm225407-art <duy_dpm225407@student.agu.edu.vn>
2025-10-06 11:08:27 +0700

    Add files via upload

---
+++ DPM225407_NguyenHuuDuy_Real_16_Iterator/IAbstractCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225407_NguyenHuuDuy_Real_16_Iterator
+{
+    public interface IAbstractCollection
+    {
+        Iterator CreateIterator();
+    }
+}
---
+++ DPM225407_NguyenHuuDuy_Real_16_Iterator/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225407_NguyenHuuDuy_Real_16_Iterator
+{
+    public class Item
+    {
+        string name;
+        // Constructor
+        public Item(string name)
+        {
+            this.name = name;
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+}
