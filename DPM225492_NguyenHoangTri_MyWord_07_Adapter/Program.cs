ce8f08c2e9b74db4a13712f6fefe0dadfc1a8519
Tri-N867019 <tri867019@gmail.com>
2025-10-06 10:45:32 +0700

    Add files via upload

---
+++ DPM225492_NguyenHoangTri_MyWord_07_Adapter/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DPM225492_NguyenHoangTri_MyWord_07_Adapter
+{
+    // Interface client muốn dùng
+    public interface IUsbDevice
+    {
+        void ConnectWithUsbCable();
+    }
+
+    // Class không tương thích (đã có sẵn)
+    public class VgaDevice
+    {
+        public void ConnectWithVgaCable()
+        {
+            Console.WriteLine("Kết nối với cáp VGA");
+        }
+    }
+
+    // Adapter giúp VgaDevice hoạt động như IUsbDevice
+    public class VgaToUsbAdapter : IUsbDevice
+    {
+        private VgaDevice _vgaDevice;
+
+        public VgaToUsbAdapter(VgaDevice vgaDevice)
+        {
+            _vgaDevice = vgaDevice;
+        }
+
+        public void ConnectWithUsbCable()
+        {
+            // Chuyển đổi gọi phương thức tương ứng
+            _vgaDevice.ConnectWithVgaCable();
+        }
+    }
+
+    // Client code
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // Bạn có 1 thiết bị VGA
+            VgaDevice vga = new VgaDevice();
+
+            // Tạo adapter để sử dụng như USB device
+            IUsbDevice usbDevice = new VgaToUsbAdapter(vga);
+
+            // Dùng thiết bị qua adapter
+            usbDevice.ConnectWithUsbCable();
+        }
+    }
+}
---
+++ DPM225492_NguyenHoangTri_MyWord_07_Bridge/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+class Program
+{
+    // Interface Implementation (nền tảng vẽ)
+    interface IDrawAPI
+    {
+        void DrawCircle(int x, int y, int radius);
+    }
+
+    // Các implementation cụ thể
+    class WindowsDrawAPI : IDrawAPI
+    {
+        public void DrawCircle(int x, int y, int radius)
+        {
+            Console.WriteLine($"Vẽ hình tròn trên Windows tại ({x},{y}) bán kính {radius}");
+        }
+    }
+
+    class LinuxDrawAPI : IDrawAPI
+    {
+        public void DrawCircle(int x, int y, int radius)
+        {
+            Console.WriteLine($"Vẽ hình tròn trên Linux tại ({x},{y}) bán kính {radius}");
+        }
+    }
+
+    // Abstraction (hình học)
+    abstract class Shape
+    {
+        protected IDrawAPI drawAPI;
+
+        protected Shape(IDrawAPI drawAPI)
+        {
+            this.drawAPI = drawAPI;
+        }
+
+        public abstract void Draw();
+    }
+
+    // Refined Abstraction (hình tròn)
+    class Circle : Shape
+    {
+        private int x, y, radius;
+
+        public Circle(int x, int y, int radius, IDrawAPI drawAPI) : base(drawAPI)
+        {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+
+        public override void Draw()
+        {
+            drawAPI.DrawCircle(x, y, radius);
+        }
+    }
+
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Shape circle1 = new Circle(10, 20, 5, new WindowsDrawAPI());
+        Shape circle2 = new Circle(15, 25, 10, new LinuxDrawAPI());
+
+        circle1.Draw();
+        circle2.Draw();
+    }
+}
