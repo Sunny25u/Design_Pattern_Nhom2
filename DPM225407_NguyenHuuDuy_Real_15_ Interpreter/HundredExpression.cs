d2c95dd06d56e6c47d736d9d312a21fdbd2689bc
duydpm225407-art <duy_dpm225407@student.agu.edu.vn>
2025-10-06 10:06:35 +0700

    Add files via upload

---
+++ DPM225407_NguyenHuuDuy_Real_15_ Interpreter/HundredExpression.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225407_NguyenHuuDuy_Real_15__Interpreter
+{
+    public class HundredExpression : Expression
+    {
+        public override string One() { return "C"; }
+        public override string Four() { return "CD"; }
+        public override string Five() { return "D"; }
+        public override string Nine() { return "CM"; }
+        public override int Multiplier() { return 100; }
+    }
+
+}
