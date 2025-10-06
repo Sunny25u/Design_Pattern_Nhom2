8afbdbd32aef24f928df0a4e4d2b74148cfd9f31
duydpm225407-art <duy_dpm225407@student.agu.edu.vn>
2025-10-06 09:20:03 +0700

    Add files via upload

---
+++ DPM225407_NguyenHuuDuy_Patterns_06_Adapter/Adaptee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225407_NguyenHuuDuy_Patterns_06_Adapter
+{
+    public class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Called SpecificRequest()");
+        }
+    }
+}
---
+++ DPM225407_NguyenHuuDuy_Patterns_06_Adapter/Program.cs
@@ -0,0 +1,12 @@
+﻿using DPM225407_NguyenHuuDuy_Patterns_06_Adapter;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create adapter and place a request
+        Target target = new Adapter();
+        target.Request();
+        // Wait for user
+        Console.ReadKey();
+    }
+}
