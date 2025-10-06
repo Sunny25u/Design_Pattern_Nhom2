b905e1e66cfd82e0ac619f036e2bde9bb433ea6e
duydpm225407-art <duy_dpm225407@student.agu.edu.vn>
2025-10-06 09:46:28 +0700

    Add files via upload

---
+++ DPM225407_NguyenHuuDuy_Real_07_Bridge/Program.cs
@@ -0,0 +1,21 @@
+﻿using DPM225407_NguyenHuuDuy_Real_07_Bridge;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create RefinedAbstraction
+        var customers = new Customers();
+        // Set ConcreteImplementor
+        customers.Data = new CustomersData("Chicago");
+        // Exercise the bridge
+        customers.Show();
+        customers.Next();
+        customers.Show();
+        customers.Next();
+        customers.Show();
+        customers.Add("Henry Velasquez");
+        customers.ShowAll();
+        // Wait for user
+        Console.ReadKey();
+    }
+}
---
+++ DPM225407_NguyenHuuDuy_Real_08_Composite/PrimitiveElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DPM225407_NguyenHuuDuy_Real_08_Composite
+{
+    public class PrimitiveElement : DrawingElement
+    {
+        public PrimitiveElement(string name)
+           : base(name)
+        {
+        }
+        public override void Add(DrawingElement c)
+        {
+            Console.WriteLine(
+                "Cannot add to a PrimitiveElement");
+        }
+        public override void Remove(DrawingElement c)
+        {
+            Console.WriteLine(
+                "Cannot remove from a PrimitiveElement");
+        }
+        public override void Display(int indent)
+        {
+            Console.WriteLine(
+                new String('-', indent) + " " + name);
+        }
+    }
+}
