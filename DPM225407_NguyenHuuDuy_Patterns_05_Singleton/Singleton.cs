95423a558e13614b3f703662f84d4d44b3ef06ae
duydpm225407-art <duy_dpm225407@student.agu.edu.vn>
2025-10-06 09:11:39 +0700

    Add files via upload

---
+++ DPM225407_NguyenHuuDuy_Patterns_05_Singleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225407_NguyenHuuDuy_Patterns_05_Singleton
+{
+    class Singleton
+    {
+        static Singleton instance;
+        // Constructor is 'protected'
+        protected Singleton()
+        {
+        }
+        public static Singleton Instance()
+        {
+            // Uses lazy initialization.
+            // Note: this is not thread safe.
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
