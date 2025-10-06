7cf673a8e4597c07cd0cb09be8d9f88359674e5c
duydpm225407-art <duy_dpm225407@student.agu.edu.vn>
2025-10-06 09:07:13 +0700

    Add files via upload

---
+++ DPM225407_NguyenHuuDuy_Patterns_03_FactoryMethod/ConcreteCreatorB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225407_NguyenHuuDuy_Patterns_03_FactoryMethod
+{
+    class ConcreteCreatorB : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductB();
+        }
+    }
+}
