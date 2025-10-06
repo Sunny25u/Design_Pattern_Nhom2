00afbd893948927aaf740a99511d0ba40a50c429
duydpm225407-art <duy_dpm225407@student.agu.edu.vn>
2025-10-06 09:01:09 +0700

    Add files via upload

---
+++ DPM225407_NguyenHuuDuy_Patterns_18_Memento/Program.cs
@@ -0,0 +1,18 @@
+﻿using DPM225407_NguyenHuuDuy_Patterns_18_Memento;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Originator o = new Originator();
+        o.State = "On";
+        // Store internal state
+        Caretaker c = new Caretaker();
+        c.Memento = o.CreateMemento();
+        // Continue changing originator
+        o.State = "Off";
+        // Restore saved state
+        o.SetMemento(c.Memento);
+        // Wait for user
+        Console.ReadKey();
+    }
+}
---
+++ DPM225407_NguyenHuuDuy_Patterns_19_Observer/Observer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225407_NguyenHuuDuy_Patterns_19_Observer
+{
+    public abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+}
