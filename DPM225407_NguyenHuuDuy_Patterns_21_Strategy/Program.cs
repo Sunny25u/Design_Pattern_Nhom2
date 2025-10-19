8915525ce4c113ba8cde31715d0b5b471dab1882
duydpm225407-art <duy_dpm225407@student.agu.edu.vn>
2025-10-20 00:18:12 +0700

    Add files via upload

---
+++ DPM225407_NguyenHuuDuy_Patterns_21_Strategy/Program.cs
@@ -0,0 +1,17 @@
+﻿using DPM225407_NguyenHuuDuy_Patterns_21_Strategy;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Context context;
+        // Three contexts following different strategies
+        context = new Context(new ConcreteStrategyA());
+        context.ContextInterface();
+        context = new Context(new ConcreteStrategyB());
+        context.ContextInterface();
+        context = new Context(new ConcreteStrategyC());
+        context.ContextInterface();
+        // Wait for user
+        Console.ReadKey();
+    }
+}
---
+++ DPM225407_NguyenHuuDuy_Real_21_Strategy/Program.cs
@@ -0,0 +1,22 @@
+﻿using DPM225407_NguyenHuuDuy_Real_21_Strategy;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Two contexts following different strategies
+        SortedList studentRecords = new SortedList();
+        studentRecords.Add("Samual");
+        studentRecords.Add("Jimmy");
+        studentRecords.Add("Sandra");
+        studentRecords.Add("Vivek");
+        studentRecords.Add("Anna");
+        studentRecords.SetSortStrategy(new QuickSort());
+        studentRecords.Sort();
+        studentRecords.SetSortStrategy(new ShellSort());
+        studentRecords.Sort();
+        studentRecords.SetSortStrategy(new MergeSort());
+        studentRecords.Sort();
+        // Wait for user
+        Console.ReadKey();
+    }
+}
